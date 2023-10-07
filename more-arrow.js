f833a770641db5567092a55f6edeade9476601d5
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-10-07 11:33:55 +0600

    more about arrow funtion

---
+++ more-arrow.js
@@ -0,0 +1,21 @@
+const add = (a,b) => a+b;
+
+const getAge = (person) => person.age
+
+const getThird = numbers => numbers[2]
+
+const getMessage = () => "Hello World"
+
+const student = {
+    name:"Kawser Ahmed",
+    age: 24
+}
+
+const sum = add(10,20)
+const age = getAge(student)
+const number = getThird([10,20,30,40])
+
+console.log(sum)
+console.log(age)
+console.log(number)
+console.log(getMessage())
