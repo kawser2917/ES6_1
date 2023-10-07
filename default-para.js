6923f569cb38acdd84354d2e8f16992a46c8452f
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-10-07 10:16:05 +0600

    es6 started with default para

---
+++ default-para.js
@@ -0,0 +1,13 @@
+function add(num1=0,num2=0){
+    const sum = num1 + num2;
+    console.log(sum)
+}
+
+// add(20,30)
+// add(20)
+add()
+
+// for number default para we will save 0 or 1
+// for string default parameter we will save empty string ''
+// for array default parameter we will save empty array []
+// for object default parameter we will save empty object {}
