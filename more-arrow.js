64e9b9a8b7c23251545199b11e7b18bf2ff478c2
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-10-07 11:34:24 +0600

    multiple line arrow funtion

--- more-arrow.js
+++ more-arrow.js
@@ -6,6 +6,12 @@ const getThird = numbers => numbers[2]
 
 const getMessage = () => "Hello World"
 
+const mulOperation = (a,b,c) =>{
+    const sum = a+b
+    const mul = sum*c
+    return mul
+}
+
 const student = {
     name:"Kawser Ahmed",
     age: 24
@@ -14,8 +20,10 @@ const student = {
 const sum = add(10,20)
 const age = getAge(student)
 const number = getThird([10,20,30,40])
+const dim = mulOperation(10,2,3)
 
 console.log(sum)
 console.log(age)
 console.log(number)
+console.log(dim)
 console.log(getMessage())
