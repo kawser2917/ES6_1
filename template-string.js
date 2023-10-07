07e3bc971ead933ae74c8f8f8f4713cead065c24
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-10-07 10:43:05 +0600

    template string

---
+++ template-string.js
@@ -0,0 +1,19 @@
+const a = 10
+const b = 30
+
+const result = "The sum of"+ " " + a + " " + "and" + b + " " + "is" + " " + (a+b)
+
+const sum = `The sum of ${a} and ${b} is ${a+b}`
+
+console.log(result)
+console.log(sum)
+
+// backtick will help also write string in new line
+
+const name = `This is Kawser
+I just want to do something 
+So that man respect me
+no one can avoid me
+One day i will be the man who ha ha ha`
+
+console.log(name)
